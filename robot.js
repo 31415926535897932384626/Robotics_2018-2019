332df4f5813e81525bc7c59016a10fbb611dd7ef
Thomas Follett <37428021+31415926535897932384626@users.noreply.github.com>
2018-11-21 16:50:16 -0800

    Add files via upload

---
+++ robot.js
@@ -0,0 +1,12 @@
+function anglicisme(){
+  document.getElementById("text").innerHTML = "I LIKE ROBOTS AND I CANNOT LIE...";
+  document.getElementById("logoText").innerHTML = "Jules-Verne's Robotics";
+  document.getElementById("enFrancais").style.display = "block";
+  document.getElementById("toEnglish").style.display = "none";
+}
+function frenchFry(){
+  document.getElementById("text").innerHTML = "J'AIME LES ROBOTS ET JE NE PEUX PAS MENTIR...";
+  document.getElementById("logoText").innerHTML = "Robotique Jules-Verne";
+  document.getElementById("toEnglish").style.display = "block";
+  document.getElementById("enFrancais").style.display = "none";
+}
