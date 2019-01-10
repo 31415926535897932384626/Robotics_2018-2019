4d446da1c68b4a865aca8be1205bc0f72499289e
Thomas Follett <37428021+31415926535897932384626@users.noreply.github.com>
2019-01-09 16:42:13 -0800

    Add files via upload

---
+++ text.js
@@ -0,0 +1,13 @@
+var text = {
+  "JVFrench":"Jules-Vernes est une é&eacuteole francophone secondaire au coeur de Vancouver, Colombie-Britannique. </br>\
+     Trouvé dans le quartier de Cambie Sud, cette magnifique &eacutecole est nommé après l’écrivain célèbre Jules Verne.\
+     Cette école est un des plusieurs qui accepte le programme Baccalaur&eacuteat International. \
+     On accueille des &eacutelèves de toutes les cultures, de 7ème à la 12ème année. Notre &eacutecole &eacutetait construit en 2009.\
+     L’école J-V a une mascotte qui s’appelle l’Albatros avec des couleurs de gris et d’or.\
+     Les valeurs de cette école sont le respect, la persévérance, la responsabilité, l’intégrité et l’inclusion.\
+     Le site web de l’école est ici: <a href='https://julesverne.csf.bc.ca'> Jules-Verne <a>",
+  "JVEnglish":"Blah biddy english crap....",
+
+  "HomeEnglish":"Welcome to our site.",
+  "HomeFrench":"Bienvenue &agrave notre site.",
+}
