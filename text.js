563b918255a2320a64b612ef957e07d138a2a476
31415926535897932384626433832795028841 <46544890+31415926535897932384626433832795028841@users.noreply.github.com>
2019-01-09 16:45:15 -0800

    Add files via upload

---
+++ text.js
@@ -0,0 +1,32 @@
+var text = {
+  "JVFrench":"Jules-Vernes est une &eacutecole francophone secondaire au coeur de Vancouver, Colombie-Britannique.</br>\
+     Trouv&eacute dans le quartier de Cambie Sud, cette magnifique &eacutecole est nommé après l’écrivain célèbre Jules Verne.</br>\
+     Cette &eacutecole est une ce celles qui acceptent le programme Baccalaur&eacuteat International.</br> \
+     On accueille des &eacutel&egraveves de toutes les cultures, de 7&egraveme à la 12&egraveme ann&eacutee. Notre &eacutecole &eacutetait construit en 2009.</br>\
+     L’&eacutecole J-V a une mascotte qui s’appelle l’Albatros avec des couleurs de gris et d’or.</br>\
+     Les valeurs de cette école sont le respect, la pers&eacutev&eacuterance, la responsabilit&eacute, l’intégrit&eacute et l’inclusion.</br>\
+     Le site web de l’&eacutecole est ici: <a href='https://julesverne.csf.bc.ca'> Jules-Verne </a> </br>",
+
+  "JVEnglish":"Jules-Vernes is a secondary French school in the heart of Vancouver, British-Columbia.</br>\
+   Found in the district of Cambie South, this magnificent school is named after the famous writer Jules Verne.</br>\
+   It is one of several that accepts the International Baccalaureate program.</br>\
+   We welcome students from all cultures, from 7th to 12th grade. Our school was built in 2009.</br>\
+   The school J-V has a mascot named the Albatross with colours of gray and gold.</br>\
+   The values ​​of this school are Respect, Perseverance, Responsibility, Integrity and Inclusion.</br>\
+  The website is here : <a href='https://julesverne.csf.bc.ca'> Jules-Verne </a> </br>",
+
+  "JVregles":"Le jeu nécessite que les robots de différentes écoles marquent le plus de points en travaillant en équipe.</br>\
+   Donner à chaque école une chance égale de démontrer sa conception, sa construction et ses compétences de jeu stratégiques.</br>\
+   Les Équipes Deux équipes, deux robots, bleu et jaune l'un contre l'autre à chaque tour.</br>\
+   Les robots changeront de partenaire d'un tour à l'autre.</br>\
+   Terrain de jeu</br>\
+   Le terrain de jeu est comme un escalier.</br>\
+   Il est composé de caisses du même volume, mais elles ne sont pas positionnées de la même manière.</br>\
+   Le niveau 1 est le niveau le plus bas (le plus facile) l et le niveau 5 est le plus élevé (le plus difficile).</br></br>\
+   Les zones de départ disponibles sont situées au niveau 1, au niveau 3 et au niveau 5.</br>\
+   Les distributeurs de pièces de jeu sont situés au niveau 1, au niveau 4 et au niveau 5.</br>\
+   Les cibles sont situées au niveau 2, au niveau 3 et au niveau 4.</br>\
+        "
+
+
+}
