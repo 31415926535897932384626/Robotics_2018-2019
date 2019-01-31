3056d1fe3d27d801bbb4db3f2e523ed437225084
Thomas Follett <37428021+31415926535897932384626@users.noreply.github.com>
2019-01-30 16:03:14 -0800

    Update text.js

--- text.js
+++ text.js
@@ -1,32 +1,43 @@
 var text = {
-  "JVFrench":"Jules-Vernes est une &eacutecole francophone secondaire au coeur de Vancouver, Colombie-Britannique.</br>\
-     Trouv&eacute dans le quartier de Cambie Sud, cette magnifique &eacutecole est nommé après l’écrivain célèbre Jules Verne.</br>\
-     Cette &eacutecole est une ce celles qui acceptent le programme Baccalaur&eacuteat International.</br> \
-     On accueille des &eacutel&egraveves de toutes les cultures, de 7&egraveme à la 12&egraveme ann&eacutee. Notre &eacutecole &eacutetait construit en 2009.</br>\
-     L’&eacutecole J-V a une mascotte qui s’appelle l’Albatros avec des couleurs de gris et d’or.</br>\
-     Les valeurs de cette école sont le respect, la pers&eacutev&eacuterance, la responsabilit&eacute, l’intégrit&eacute et l’inclusion.</br>\
-     Le site web de l’&eacutecole est ici: <a href='https://julesverne.csf.bc.ca'> Jules-Verne </a> </br>",
-  "JVEnglish":"Jules-Vernes is a secondary French school in the heart of Vancouver, British-Columbia.</br>\
-    Found in the district of Cambie South, this magnificent school is named after the famous writer Jules Verne.</br>\
-    It is one of several that accepts the International Baccalaureate program.</br>\
-    We welcome students from all cultures, from 7th to 12th grade. Our school was built in 2009.</br>\
-    The school J-V has a mascot named the Albatross with colours of gray and gold.</br>\
-    The values of this school are Respect, Perseverance, Responsibility, Integrity and Inclusion.</br>\
-    The website is here : <a href='https://julesverne.csf.bc.ca'> Jules-Verne </a> </br>",
+  "JVFrench":"Jules-Vernes est une &eacutecole francophone secondaire au coeur de Vancouver, </br>\
+              Colombie-Britannique.  Trouv&eacute dans le quartier de Cambie Sud, cette </br>\
+              magnifique &eacutecole est nommé après l’écrivain célèbre Jules Verne. Cette </br>\
+              &eacutecole est une ce celles qui acceptent le programme Baccalaur&eacuteat </br>\
+              International. On accueille des &eacutel&egraveves de toutes les cultures, de </br>\
+              7&egraveme à la 12&egraveme ann&eacutee. Notre &eacutecole &eacutetait construit </br>\
+              en 2009. L’&eacutecole J-V a une mascotte qui s’appelle l’Albatros avec des </br>\
+              couleurs de gris et d’or. Le site web de l’&eacutecole est ici:\
+               <a href='https://julesverne.csf.bc.ca'> Jules-Verne </a>  ",
+  "JVEnglish":"Jules-Vernes is a secondary French school in the heart of Vancouver, </br>\
+              British-Columbia. Found in the district of Cambie South, this magnificent </br>\
+              school is named after the famous writer Jules Verne. It is one of several that </br>\
+              accepts the International Baccalaureate program. We welcome students from all </br>\
+              cultures, from 7th to 12th grade. Our school was built in 2009. The school J-V </br>\
+              has a mascot named the Albatross with colours of gray and gold. The values of </br>\
+              this school are respect, perseverance, responsibility, integrity, and inclusion. </br> \
+              The website is here : <a href='https://julesverne.csf.bc.ca'> Jules-Verne </a>  ",
 
   "HomeEnglish":"Welcome to our site.",
   "HomeFrench":"Bienvenue sur notre site.",
 
-  "JVregles":"Le jeu nécessite que les robots de différentes écoles marquent le plus de points en travaillant en équipe.</br>\
-   Donner à chaque école une chance égale de démontrer sa conception, sa construction et ses compétences de jeu stratégiques.</br>\
-   Les Équipes Deux équipes, deux robots, bleu et jaune l'un contre l'autre à chaque tour.</br>\
-   Les robots changeront de partenaire d'un tour à l'autre.</br>\
-   Terrain de jeu</br>\
-   Le terrain de jeu est comme un escalier.</br>\
-   Il est composé de caisses du même volume, mais elles ne sont pas positionnées de la même manière.</br>\
-   Le niveau 1 est le niveau le plus bas (le plus facile) l et le niveau 5 est le plus élevé (le plus difficile).</br></br>\
-   Les zones de départ disponibles sont situées au niveau 1, au niveau 3 et au niveau 5.</br>\
-   Les distributeurs de pièces de jeu sont situés au niveau 1, au niveau 4 et au niveau 5.</br>\
-   Les cibles sont situées au niveau 2, au niveau 3 et au niveau 4.</br>"
+  "CRCFrench":"CRC robotiques est un groupe de jeunes professionnels et enseignants formé en 2001. Chaque membre partage les même passions;\
+   la robotique et l’éducation. Déçu par les parascolaires disponibles pour les écoles, CRC robotiques planifia un concours annuel alliant \
+   les sciences, les technologies, l’ingénierie et les mathématiques, l’informatique, les arts, et les langues. Cette année, le concours est\
+   appelé Vertigoal! </br> Vertigoal va se passer au Secondaire Régional De Centennial du 21 au 23 février 2019. </br> La compétition sera noté sur\
+   ces quatre sujets :\
+   Robot : Chaque équipe doit construire un robot. Le but du robot est de marquer ses balles de tennis dans des paniers. L’équipe avec les plus\
+   de balles de leur couleur dans les paniers gagne !\
+   Kiosque : Chaque équipe doit construire un kiosque. L’équipe du kiosque le plus original gagne !\
+   Vidéo : Chaque équipe doit monter une vidéo d’eux même. La vidéo la plus mémorable gagne !\
+   Site Web : Chaque équipe doit créer un site web avec les informations nécessaires. Le site web le plus organisé gagne !",
 
+  "CRCEnglish": "CRC robotics is a group of young professionals and teachers formed in 2001. Each member shares the same passions; robotics and education.\
+    Disappointed by the extracurricular activities available for schools, CRC robotics planned an annual competition combining science, technology,\
+    engineering and mathematics, computer science, the arts, and languages. This year, the contest is called Vertigoal! </br>\
+    Vertigoal 2019 will take place at the Centennial Regional High School in Montreal from the 21 till the 23 of february, 2019. </br>\
+    The competition will be noted on these four subjects :</br>\
+    Robot : Each team must build a robot. The robot's goal is to shoot its tennis balls into baskets. The team with the most balls of their color in the baskets wins! </br>\
+    Booth : Each team must build a booth. The team from the most original booth wins! </br>\
+    Video : Each team must make a video of themselves. The video the most memorable wins! </br>\
+    Website : Each team must create a website with the necessary information. The web site the most organised wins!",
 }
